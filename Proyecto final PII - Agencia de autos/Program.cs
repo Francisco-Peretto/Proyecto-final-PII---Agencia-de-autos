40e105ad8da90fa82d7f7d0ef3d7dcae9d1fc6c4
Genaro Noel Vallini <gvallini@frsn.utn.edu.ar>
2024-10-15 23:12:13 -0300

    ARREGLADOS TXTS

--- Proyecto final PII - Agencia de autos/Program.cs
+++ Proyecto final PII - Agencia de autos/Program.cs
@@ -12,17 +12,17 @@ namespace Proyecto_final_PII___Agencia_de_autos
         {
             // testeo de push --- Franco 
             
-            string vehiculosArchivo = "Vehiculos.txt";
-            string ventasArchivo = "Ventas.txt";
-            string clientesArchivo = "Clientes.txt";
-            string marcasArchivo = "Marcas.txt";
-            string segmentosArchivo = "Segmentos.txt";
-            string combustiblesArchivo = "Combustibles.txt";
-            string localidadesArchivo = "Localidades.txt";
-            string provinciasArchivo = "Provincias.txt";
-            string motosArchivo = "Motos.txt";
-            string autosCamionetasArchivo = "AutosCamioneta.txt";
-            string camionesArchivo = "Camiones.txt";
+            //string vehiculosArchivo = "Vehiculos.txt";
+            //string ventasArchivo = "Ventas.txt";
+            //string clientesArchivo = "Clientes.txt";
+            //string marcasArchivo = "Marcas.txt";
+            //string segmentosArchivo = "Segmentos.txt";
+            //string combustiblesArchivo = "Combustibles.txt";
+            //string localidadesArchivo = "Localidades.txt";
+            //string provinciasArchivo = "Provincias.txt";
+            //string motosArchivo = "Motos.txt";
+            //string autosCamionetasArchivo = "AutosCamioneta.txt";
+            //string camionesArchivo = "Camiones.txt";
 
 
             Concesionaria con = new Concesionaria ();
@@ -38,34 +38,22 @@ namespace Proyecto_final_PII___Agencia_de_autos
             //con.CargarMotos(motosArchivo);
             //con.CargarAutosCamionetas(autosCamionetasArchivo);
             //con.CargarCamiones(camionesArchivo);
-            con.CargarVehiculos(vehiculosArchivo);
+            //con.CargarVehiculos(vehiculosArchivo);
             con.CargarVentas();
             con.CargarClientes();
-            con.CargarMarcas(marcasArchivo);
-            con.CargarSegmentos(segmentosArchivo);
-            con.CargarCombustibles(combustiblesArchivo);
-            con.CargarLocalidades(localidadesArchivo);
-            con.CargarProvincias(provinciasArchivo);
+            con.CargarMarcas();
+            con.CargarSegmentos();
+            con.CargarCombustibles();
+            con.CargarLocalidades();
+            con.CargarProvincias();
             con.CargarMotos();
             con.CargarAutosCamionetas();
             con.CargarCamiones();
-            con.actualizarProvincias();
-            con.actualizarCombustibles();
-            con.actualizarLocalidades();
-            con.actualizarSegmentos();
-            con.ActualizarAutoCamioneta();
-            con.ActualizarCamiones();
-            con.ActualizarClientes();
-            con.ActualizarMarcas();
-            con.ActualizarMotos();
-            con.ActualizarVentas();
-
+            
 
 
             AutoCamioneta ac = new AutoCamioneta();
             Menu menu = new Menu ();
-            Console.Write($"{ac.pPatente}");
-            Console.ReadKey();
             menu.MenuPrincipal();
             /*
 int opc;
